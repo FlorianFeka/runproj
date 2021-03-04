08b554a6e86c8695a0c8a40777b667491b784a7f
Florian Feka <47530143+FlorianFeka@users.noreply.github.com>
2021-03-04 22:48:17 +0100

    add basic read json file

--- runproj.go
+++ runproj.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type set struct {
+	Name string `json:"name"`
+	Programs []program `json:"programs"`
+}
+
+type program struct {
+	ProgramPath string `json:"program"`
+	Path string `json:"path"`
+}
 
 func main() {
-	fmt.Println("Nothing")
+	confFile, err := os.Open("./.ignore/runproj.json")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer confFile.Close()
+	
+	byteConf, _ := ioutil.ReadAll(confFile)
+
+	var set []set
+
+	json.Unmarshal(byteConf, &set)
+
+	for i:=0; i < len(set); i++ {
+		fmt.Println(set[i].Name)
+		for j:=0; j<len(set[i].Programs) ;j++ {
+			fmt.Println(set[i].Programs[j].ProgramPath)
+			fmt.Println(set[i].Programs[j].Path)
+		}
+	}
 }
